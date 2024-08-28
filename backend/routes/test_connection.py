f75090737cb731e9401d703250aaa85300676e4b
Albert Linan <alblinmah@gmail.com>
2024-08-28 19:40:45 +1000

    fist_stable_version

--- backend/routes/test_connection.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from database import db
-
-router = APIRouter()
-
-@router.get("/test-connection")
-async def test_connection():
-    return {"status": "Connected to MongoDB Atlas!"}
